8e53e559094344db7027ce662d5d1a39197c05d0
vanderzee94 <vandez@rpi.edu>
2014-10-05 16:35:39 -0400

    Changed process class and started working in shortest job first

--- hw2.py
+++ hw2.py
@@ -8,22 +8,56 @@ Caitlin Connerney
 '''
 
 import random
+import operator
 
 # Process Class
 class process:
-	pid 			= 0		# Process ID
-	burst 		= 0		# Time it takes to complete
-	turn 			= 0		# Total Time taken to complete
-	wait 			= 0		# Time spent in Queue
-	priority 	= 0		# Priority Level
+	# Constructor for a Process
+	def __init__(self, p, t, b, i):
+		self.pid 	= p      		# Process ID
+		self.time = t 				# Time it takes to complete 20-200ms or 200-3000ms
+		self.turn = 0					# Total Time taken to complete
+		self.wait = 0					# Time spent in Queue
+		self.priority = 0			# Priority Level
+		self.bursts = b 			# Bursts it takes to complete
+		self.inter = i 				# Interactive Process or not
+
+	# Simple print function for debugging
+	def printP(self):
+		print(self.pid,self.time,self.turn,self.wait,self.priority,self.bursts,self.inter)
+	
+	# Returns whether a process has finished or not
+	def done(self):
+		if self.bursts == 0:
+			True
+		else:
+			False
+
 
 
 #	Shortest Job First without Preemption algorithm
 # Takes in a list of processes and puts them into a que to run
-def SJFN( processes, time ):
+def SJFN( processes ):
+	# Put all processes in Queue
+	queue = []
+	for p in processes:
+		queue.append(p)
+	# Sort the list by the completion time
+	queue.sort(key=operator.attrgetter('time'))
+	for p in queue:
+		if p.inter:
+			print "[time 0ms] Interactive process ID ", p.pid, " entered ready queue (requires ", p.time, "ms CPU time)"
+		else:
+			print "[time 0ms] CPU-bound process ID ", p.pid, " entered ready queue (requires ", p.time, "ms CPU time)"
+	# Simulate processing
+	time = 0
+	while queue.length != 0:
+		# Run the process on top of the queue
+		print "Process "
+
 	
 
-
+'''
 #	Shortest Job First with Preemption algorithm
 # Takes in a list of processes and puts them into a que to run
 def	SJFP( processes, time ):
@@ -49,3 +83,15 @@ def inc( processes ):
 #		int m = number of CPUs. Should be be 1 or 4
 #		int n = number of Processes. The default value is 12
 def simulate( m, n ):
+
+'''
+
+def main():
+	one 	= process(1,100,1,True)
+	two 	= process(2,3000,8,False)
+	three = process(3,350,8,False)
+	processes = [one,two,three]
+	SJFN(processes)
+
+if __name__ == '__main__': 
+	main()
